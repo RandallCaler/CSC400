e68fa677371a2b61529469a902bab840ef69b34a
hgarmany <hlgarmany@gmail.com>
2024-04-30 12:02:39 -0700

    shader patch
    restored cell shading to tex shader

--- resources/tex_frag0.glsl
+++ resources/tex_frag0.glsl
@@ -37,5 +37,5 @@ void main() {
 	// total reflective profile
 	color = vec4(MatAmb + specular*MatSpec + diffuse*MatDif, 1.0) * texColor;
   //if (flip > 0)
-  	//color = floor(color * 4 + 4) / 8.7; // using cel shading for cat!
+  	color = floor(color * 4 + 4) / 8.7; // using cel shading for cat!
 }
