34a454b4be45ebabbc8dc25f1692b8149ffd7226
hgarmany <hlgarmany@gmail.com>
2024-05-01 13:47:41 -0700

    CheckGroundCollision
    collider supports ground collision checks
    input: heightmap, an origin around which the plane is centered in the xz plane and at 0 on the y axis, and information about the scale of the plane
    returns: float, y position of the ground at the location

--- src/Collider.h
+++ src/Collider.h
@@ -3,6 +3,7 @@
 #ifndef COLLIDER_H_INCLUDED
 #define COLLIDER_H_INCLUDED
 
+#include "Texture.h"
 #include "Shape.h"
 
 class Entity; // forward declaration to enable use of this class in Entity class
@@ -25,6 +26,7 @@ public:
     Collider();
     Collider(Entity *owner);
     void UpdateColliderSize();
+    float CheckGroundCollision(std::shared_ptr<Texture> hMap, glm::vec3 hMapOrigin, glm::vec3 scale);
     int CheckCollision(std::vector<Entity>& entities);
     void SetEntityID(int ID);
     bool IsColliding();
@@ -40,6 +42,7 @@ public:
 private:
 
     int entityId;
+    Entity* owner;
     float radial;
     bool colliding = false;
 
