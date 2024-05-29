847c70b9761188fa2d6084577c8cff267328b3df
nidhiraviprasad <nidhi.raviprasad@gmail.com>
2024-05-29 16:57:51 -0700

    level changes

--- resources/hmap_frag.glsl
+++ resources/hmap_frag.glsl
@@ -20,7 +20,7 @@ in OUT_struct {
 float TestShadow(vec4 LSfPos) {
 
   //0.005 * tan (acos(nDotl)) is better/more precise
-  float depth_buffer = 0.009;
+  float depth_buffer = 0.0;
 
 	//1: shift the coordinates from -1, 1 to 0, 1
   vec3 fLS = (vec3(LSfPos) + vec3(1.0)) * 0.5;
